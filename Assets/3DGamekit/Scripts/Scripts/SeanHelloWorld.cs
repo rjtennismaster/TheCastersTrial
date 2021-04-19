d3bfeb6ee80d762fc350bfef0a36aa128262f918
Sean Twomey <56229866+SeanTwomey9@users.noreply.github.com>
2021-04-19 10:54:22 -0400

    testing my connection to repository by adding hello world file

---
+++ Assets/3DGamekit/Scripts/Scripts/SeanHelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeanHelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is a test to see if I can commit to the GitHub repository
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
