3717bc0cb1feed16a31fbc68c28a114139e25446
Randall John <randall.john36@gmail.com>
2021-04-18 20:03:12 -0400

    Added Hello World Script

---
+++ Assets/3DGamekit/Scripts/Scripts/HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    //WHAT IS UP SAMIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIR
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
