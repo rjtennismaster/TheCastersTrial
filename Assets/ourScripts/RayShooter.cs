35ce1cf237e3c9b10d27b7361a5a9ab5470866a5
Samir Hede <60902907+shede97@users.noreply.github.com>
2021-04-27 06:58:12 -0700

    Minor changes in Fireball
    Now trail of fireballs doesn't disappear if player is firing while in infozone.

--- Assets/ourScripts/RayShooter.cs
+++ Assets/ourScripts/RayShooter.cs
@@ -16,12 +16,6 @@ public class RayShooter : MonoBehaviour
 	void Start()																						
 	{
 		_camera = GetComponent<Camera>();
-
-		//_animator = GameObject.Find("Player").GetComponent<Animator>();
-
-		//Cursor.lockState = CursorLockMode.Locked;
-		//Cursor.visible = false;
-
 	}
 
 	/** Deprecated in Unity 2018 
@@ -43,16 +37,6 @@ public class RayShooter : MonoBehaviour
 			_fireball.transform.rotation = transform.rotation;
 			Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
 			Ray ray = _camera.ScreenPointToRay(point);
-			RaycastHit hit;
-			/**if (Physics.Raycast(ray, out hit))
-			{
-				GameObject hitObject = hit.transform.gameObject;
-				ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
-				if (target != null)
-				{
-					target.ReactToHit();
-				}
-			}**/
 		}
 	}
 
