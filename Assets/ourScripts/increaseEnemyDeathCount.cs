60b67911a57976926cdec7851735ebc1e993b1ad
Sean Twomey <56229866+SeanTwomey9@users.noreply.github.com>
2021-04-27 13:09:44 -0400

    Added enemies remaining counter

--- Assets/ourScripts/increaseEnemyDeathCount.cs
+++ Assets/ourScripts/increaseEnemyDeathCount.cs
@@ -8,5 +8,6 @@ public class increaseEnemyDeathCount : MonoBehaviour
     public void increaseEnemyCounter()
     {
         GameObject.Find("Ellen").GetComponent<killall>().increaseEnemiesDeadCounter();
+        enemiesRemaining.remainingEnemies = 7- GameObject.Find("Ellen").GetComponent<killall>().enemiesDeadCounter;
     }
 }
